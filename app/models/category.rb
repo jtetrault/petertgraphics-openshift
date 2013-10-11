e0e3d125c57388c7766f1113d5a3f784c781b27a
Joel Tetrault <star99ers@gmail.com>
2013-10-11 16:32:41 -0500

    progress

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   attr_accessible :description, :title
   has_many :posts
+  validates :title, uniqueness: {case_sensitive: false}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ActiveRecord::Base
   belongs_to :category
   attr_accessible :description, :title, :image, :category_id
   has_attached_file :image, :styles => {:large => "600x600", :medium => "300x300", :thumb => "100x100"}
+  validates :category, :title, :image, presence: true
+
+  def short_description
+  	result = description[0, 200]
+  	if description.length > 200
+  		result += "..."
+  	end
+  	return result
+  end
+
 end
