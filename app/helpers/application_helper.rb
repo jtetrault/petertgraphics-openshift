3d4a2eafa9412f519a2db3d6806db4e7a6ba6cd7
Joel Tetrault <star99ers@gmail.com>
2013-10-12 11:28:42 -0500

    list all posts on index added cancel_button and back_button helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+	include Rails.application.routes.url_helpers
+
+	# Returns a cancel button that links to whatever url link contains,
+	# or the previous page if no link is specified.
+	# The button will perform a confirmation before following the link.
+	#
+	def cancel_button (link = nil)
+		if link
+			return link_to "Cancel", link, :class => "btn"
+		end
+		return link_to "Cancel", request.env["HTTP_REFERER"], :class => "btn", :confirm => "Are you sure? Any changes will be lost."
+	end
+
+	# Returns a back button that links to whatever url link contains,
+	# or the previous page if no link is specified.
+	#
+	def back_button (link = nil)
+		if link
+			return link_to "Back", link, :class => "btn"
+		end
+		return link_to "Back", request.env["HTTP_REFERER"], :class => "btn"
+	end
 end
