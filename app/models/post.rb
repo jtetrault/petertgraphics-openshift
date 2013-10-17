8d6525bd1011ede0a4b8e5f6a4f41a95efb861e0
Joel Tetrault <star99ers@gmail.com>
2013-10-17 16:52:06 -0500

    allow files uploaded with paperclip to not be wiped out after deploys

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :category
   attr_accessible :description, :title, :image, :category_id
-  has_attached_file :image, :styles => {:large => "600x600", :medium => "300x300", :thumb => "100x100"}
+  has_attached_file :image, :styles => {:large => "600x600", :medium => "300x300", :thumb => "100x100"},
+    :url => "/assets/paperclip/:id/:style/:id.:extension", 
+    :path => ":rails_root/public/assets/paperclip/:id/:style/:id.:extension"
   validates :category, :title, :image, presence: true
 
   def short_description
--- app/models/website_settings.rb
+++ app/models/website_settings.rb
@@ -1,5 +1,7 @@
 class WebsiteSettings < ActiveRecord::Base
 	acts_as_singleton
 	attr_accessible :contact, :homepage_blurb, :banner
-	has_attached_file :banner, :styles => {:banner => "750x300"}
+	has_attached_file :banner, :styles => {:banner => "750x300"},
+		:url => "/assets/paperclip/:id/:style/:id.:extension", 
+		:path => ":rails_root/public/assets/paperclip/:id/:style/:id.:extension"
 end
