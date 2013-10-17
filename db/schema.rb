4ce3ba66f441f88da8104e6cba2bf2632c05d8ca
Joel Tetrault <star99ers@gmail.com>
2013-10-16 23:55:40 -0500

    admin flag and login option on menu

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131017044305) do
+ActiveRecord::Schema.define(:version => 20131017044530) do
 
   create_table "categories", :force => true do |t|
     t.string   "title"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20131017044305) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
