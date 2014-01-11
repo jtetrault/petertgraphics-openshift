7f5dce33906551a24fb773313f67fe6cc1b91878
Joel Tetrault <star99ers@gmail.com>
2014-01-11 14:07:06 -0600

    gallery now uses 'href' instead of 'data-href' to create the link for the large image

--- app/assets/javascripts/gallery.js
+++ app/assets/javascripts/gallery.js
@@ -2,7 +2,7 @@ $(document).ready(function()
 	{
 		$("a.replace-image").click(function()
 			{
-				$("#link").attr("href", $(this).attr("data-href"));
+				$("#link").attr("href", $(this).attr("href"));
 				$("#image").attr("src", $(this).attr("data-image"));
 				return false;
 			}
